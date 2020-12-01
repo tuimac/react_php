2813c7498a1dbaa9a3d69c3decc733bf2e086f4e
root <tuimac.devadm01@gmail.com>
2020-12-01 04:24:51 +0000

    create react application skelton

--- init.sh
+++ init.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 PHP_PJT='php_pjt'
+REACT_PJT='react_pjt'
 
 if [ ! -e $PHP_PJT ]; then
     composer create-project --prefer-dist cakephp/app ${PHP_PJT}
     cd ${PHP_PJT}
     composer require aws/aws-sdk-php
+fi
+
+if [ ! -e $REACT_PJT ]; then
+   npx create-react-app $REACT_PJT 
 fi

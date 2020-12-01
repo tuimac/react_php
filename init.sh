f68af41909e725d426220920ef5dfae9aa6a2b7a
EC2 Default User <ec2-user@docker.tuimac.private>
2020-12-01 03:23:43 +0000

    fixed initial flow

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 PHP_PJT='php_pjt'
 
-composer create-project --prefer-dist cakephp/app ${PHP_PJT}
-cd ${PHP_PJT}
-composer require aws/aws-sdk-php
+if [ ! -e $PHP_PJT ]; then
+    composer create-project --prefer-dist cakephp/app ${PHP_PJT}
+    cd ${PHP_PJT}
+    composer require aws/aws-sdk-php
+fi

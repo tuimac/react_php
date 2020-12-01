2288f8a1a924ce7d4334eed79ffb77ce863936bc
EC2 Default User <ec2-user@docker.tuimac.private>
2020-12-01 02:22:04 +0000

    create project for PHP has been done.

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-composer create-project --prefer-dist cakephp/app php
+PHP_PJT='php_pjt'
+
+composer create-project --prefer-dist cakephp/app ${PHP_PJT}
+cd ${PHP_PJT}
+composer require aws/aws-sdk-php

dd9fdeb24db84707b1755bd90db4038686c06236
tuimac <tuimac.devadm01@gmail.com>
2020-12-13 13:02:50 +0000

    after bin/cake server and change port

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ if [ ! -e $PHP_PJT ]; then
     composer create-project --prefer-dist cakephp/app ${PHP_PJT}
     cd ${PHP_PJT}
     composer require aws/aws-sdk-php
+    composer install
 fi
 
 if [ ! -e $REACT_PJT ]; then
